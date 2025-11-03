c30849353e747c07befe55ad81057570b401138e
Family <mh5212357@gmail.com>
2025-11-03 12:09:23 -0500

    updated

--- src/main/java/com/pluralsight/FixedList.java
+++ src/main/java/com/pluralsight/FixedList.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
-public class FixedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items;
+    private int maxiSize;
+
+    public FixedList(int maxiSize) {
+        this.maxiSize = maxiSize;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        if (items.size() < maxiSize) {
+            items.add(item);
+        } else {
+            System.out.println("Do not add number");
+        }
+    }
+    public List<T> getItems(){
+        return items;
+    }
 }
+
